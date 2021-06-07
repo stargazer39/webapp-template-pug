5ddab7b797324fd48118e0b7c64da1397a2994e0
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-06-07 11:55:29 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import path from "path";
+const app = express();
+const port = 8080; // default port to listen
+
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "pug" );
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.render( "index",{
+        title : "Hi",
+        extra : "Hopfully this will work",
+        functions : [{
+            name : "Link to youtube",
+            path : "/link2yt"
+        },{
+            name : "Stream to youtube",
+            path : "/stream2yt"
+        }]
+    });
+} );
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
